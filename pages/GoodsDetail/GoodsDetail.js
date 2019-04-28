977b52d6fc31ed7b666bbf330628633feca5c7c6
AngryTope <530330847@qq.com>
2019-04-28 20:38:14 +0800

    GoodDetail page

--- pages/GoodsDetail/GoodsDetail.js
+++ pages/GoodsDetail/GoodsDetail.js
@@ -22,7 +22,20 @@ Page({
     goodSalesVolume: 8,
     imgModeScale: 'scaleToFill',
     imgModeWidthFix: 'widthFix',
-    goodDetailInfo: '商品详情'
+    goodDetailInfo: '商品详情',
+    goodDetailImg: [
+      {
+        imgSrc: '../../static/images/ShoppingMall/imgdetail.jpg'
+      }
+    ],
+    mallIconSrc: '../../static/images/ShoppingMall/ShoppingMall.png',
+    txtMallIcon: '商城',
+    consultIconSrc: '../../static/images/ShoppingMall/consult.png',
+    txtConsultIcon: '咨询',
+    cartIconSrc: '../../static/images/ShoppingMall/ShoppingCart.png',
+    txtCartIcon: '购物车',
+    addCart: '加入购物车',
+    buyNow: '立即购买'
   },
   onLoad: function (options) {
     this.setData({
